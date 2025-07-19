a6ed360faad80e8abf31858e9b86760e96b7c307
Sangini Verma <sanginiv11@gmail.com>
2025-07-19 16:26:07 +0530

    Add files via upload

---
+++ largestnumin3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, num3;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "Enter third number: ";
+    cin >> num3;
+
+    if(num1 > num2) {
+        if(num1 > num3) {
+            cout << "The largest number is: " << num1 << endl;
+        } else {
+            cout << "The largest number is: " << num3 << endl;
+        }
+    } else {
+        if(num2 > num3) {
+            cout << "The largest number is: " << num2 << endl;
+        } else {
+            cout << "The largest number is: " << num3 << endl;
+        }
+    }
+    return 0;
+}
