90faf7f8e27a216df591423d3ddf2ac08260a01b
Sangini Verma <sanginiv11@gmail.com>
2025-07-19 16:53:21 +0530

    Add files via upload

---
+++ myswitchcaseprg.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int choice;
+    cout<<"1.snacks"<<endl<<"2.meal"<<endl<<"3.beverages"<<endl;
+    cout<<"enter your choice: ";
+    cin>>choice;
+    switch(choice) {
+       case 1:
+            cout<<"1.samosa"<<endl<<"2.vadapav"<<endl<<"3.maggi"<<endl;
+            switch(choice) {
+                case 1:
+                cout<<"samosa";
+                break;
+                case 2:
+                cout<<"vadapav";
+                break;
+                case 3:
+                cout<<"maggi";
+                break;
+            }
+        break;    
+        
+        case 2:
+            cout<<"1.veg"<<endl<<"2.non-veg"<<endl;
+            switch(choice) {
+                case 1:
+                cout<<"veg";
+                break;
+                case 2:
+                cout<<"non-veg";
+                break;
+            }
+        break;
+        case 3:
+            cout<<"1.tea"<<endl<<"2.coffee"<<endl<<"3.cold drink"<<endl;
+            switch(choice) {
+                case 1:
+                cout<<"tea";
+                break;
+                case 2:
+                cout<<"coffee";
+                break;
+                case 3:
+                cout<<"cold drink";
+                break;
+                
+            }
+        break;    
+    }
+    return 0;
+}
